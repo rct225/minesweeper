ed43c7c48cfac9adc76c9ec75fe289fe7eceb94c
Robert Tuck <robtuck@gmail.com>
2017-08-25 08:55:39 -0400

    making board more dynamic by adding printBoard function

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,14 +1,14 @@
-const blankLine = '  |   |  ';
-const guessLine = '1 |   |  ';
-const bombLine  = '  | B |  ';
+const printBoard = board => {
+  console.log('Current Board:');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
 
+let board = [
+    [' ',' ',' '],
+    [' ',' ',' '],
+    [' ',' ',' ']
+  ];
 
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+printBoard(board);
