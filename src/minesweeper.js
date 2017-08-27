fb72d10317e56decffacb24222d54f2f0fb0354c
Robert Tuck <robtuck@gmail.com>
2017-08-27 13:22:24 -0400

    added code to generatePlayerBoard function

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,3 +1,11 @@
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
-
+  let board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let row = [];
+    for (let colIndex = 0; colIndex < numberOfColumns; colIndex++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
 };
