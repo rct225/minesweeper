932342b6888059958079dc42006505e56b2a1f67
Robert Tuck <robtuck@gmail.com>
2017-09-03 13:29:58 -0400

    added playMove to Game class

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -2,6 +2,20 @@ class Game {
   contstructor(numberOfRows, numberOfColumns, numberOfBombs) {
     this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs)
   }
+
+  playMove(rowIndex, columnIndex) {
+    this._board.flipTile(rowIndex, columnIndex);
+    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
+      console.log('Game over!');
+      this._board.print();
+    } else if !(this._board.hasSafeTiles()) {
+      console.log('You won!');
+    } else {
+      console.log('Current Board:');
+      this._board.print();
+    }
+  }
+
 }
 
 class Board {
