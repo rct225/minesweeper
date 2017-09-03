cc4ce61607c8ffe5465064ab0e4edc78888d2f7e
Robert Tuck <robtuck@gmail.com>
2017-09-03 13:04:15 -0400

    move flipTule into Board class

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -10,7 +10,18 @@ class Board {
     return this._playerBoard;
   }
 
-  
+ flipTile = (rowIndex, columnIndex) => {
+    if (this._playerBoard[rowIndex][columnIndex] !== ' ') {
+      console.log('This tile has already been flipped!');
+      return;
+    } else if (this._bombBoard[rowIndex][columnIndex] === 'B') {
+      this._playerBoard[rowIndex][columnIndex] = 'B';
+    } else {
+      this._playerBoard[rowIndex][columnIndex] = this.getNumberOfNeighborBombs(rowIndex, columnIndex);
+    }
+    this._numberOfTiles--;
+  }
+
 }
 
 
@@ -70,16 +81,7 @@ const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
   });
 }
 
-const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
-  if (playerBoard[rowIndex][columnIndex] !== ' ') {
-    console.log('This tile has already been flipped!');
-    return;
-  } else if (bombBoard[rowIndex][columnIndex] === 'B') {
-    playerBoard[rowIndex][columnIndex] = 'B';
-  } else {
-    playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
-  }
-}
+
 
 
 let playerBoard = generatePlayerBoard(3,4);
