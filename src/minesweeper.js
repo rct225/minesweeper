10ccf2fc2b59bdab95d1b3ef10094191affc3fc9
Robert Tuck <robtuck@gmail.com>
2017-09-03 13:18:40 -0400

    moved generatePlayerBoard and generateBombBoard into Board class

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -48,43 +48,47 @@ class Board {
       console.log(this._playerBoard.map(row => row.join(' | ')).join('\n'));
   };
 
+  static generatePlayerBoard(numberOfRows, numberOfColumns) {
+    let board = [];
+    for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+      let row = [];
+      for (let colIndex = 0; colIndex < numberOfColumns; colIndex++) {
+        row.push(' ');
+      }
+      board.push(row);
+    }
+    return board;
+  };
+
+  static generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs) {
+    let board = [];
+    for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+      let row = [];
+      for (let colIndex = 0; colIndex < numberOfColumns; colIndex++) {
+        row.push(null);
+      }
+      board.push(row);
+    }
+    let numberOfBombsPlaced = 0;
+    while (numberOfBombsPlaced < numberOfBombs) {
+      let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+      let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+      if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+        board[randomRowIndex][randomColumnIndex] = 'B';
+      }
+      numberOfBombsPlaced++;
+      // Note: this loop anc place bombs over existing bombs
+    }
+    return board;
+  };
+
 }
 
 
 
-const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
-  let board = [];
-  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
-    let row = [];
-    for (let colIndex = 0; colIndex < numberOfColumns; colIndex++) {
-      row.push(' ');
-    }
-    board.push(row);
-  }
-  return board;
-};
 
-const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
-  let board = [];
-  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
-    let row = [];
-    for (let colIndex = 0; colIndex < numberOfColumns; colIndex++) {
-      row.push(null);
-    }
-    board.push(row);
-  }
-  let numberOfBombsPlaced = 0;
-  while (numberOfBombsPlaced < numberOfBombs) {
-    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
-    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-    if (board[randomRowIndex][randomColumnIndex] !== 'B') {
-      board[randomRowIndex][randomColumnIndex] = 'B';
-    }
-    numberOfBombsPlaced++;
-    // Note: this loop anc place bombs over existing bombs
-  }
-  return board;
-};
+
+
 
 
 
