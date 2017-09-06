bf3cb55fd7fab842f370191ebe4a883f24e99d81
Robert Tuck <robtuck@gmail.com>
2017-09-06 11:32:52 -0400

    moved Game class to game.js

---
+++ src/game.js
@@ -0,0 +1,19 @@
+class Game {
+  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
+    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
+  }
+
+  playMove(rowIndex, columnIndex) {
+    //console.log(typeof this._board);
+    this._board.flipTile(rowIndex, columnIndex);
+    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
+      console.log('Game over!');
+      this._board.print();
+    } else if (!this._board.hasSafeTiles()) {
+      console.log('You won!');
+    } else {
+      console.log('Current Board:');
+      this._board.print();
+    }
+  }
+}
